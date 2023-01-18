4747fea0181479469527d4327197151bbdb7db81
Muhammad Naufal <mnaufal0999@gmail.com>
2023-01-18 09:11:04 +0700

    improve rust algo

--- FizzBuzz/FizzBuzz.rs
+++ FizzBuzz/FizzBuzz.rs
@@ -2,27 +2,16 @@ fn fizzbuzz(nums: u16) -> String {
     let mut fizzbuzz_temp: String = String::new();
     for num in 1..nums {
         if num % 3 == 0 && num % 5 == 0 {
-            let num_str: &str = &num.to_string();
-            fizzbuzz_temp.push('\n');
-            // fizzbuzz_temp.push_str(f!("({num_str}) fizzbuzz")); // using fstrings
-            fizzbuzz_temp.push_str(&format!("({}) fizzbuzz", num_str));
-        } else if num % 5 == 0 {
-            let num_str: &str = &num.to_string();
-            fizzbuzz_temp.push('\n');
-            // fizzbuzz_temp.push_str(f!("({num_str}) buzz")); // using fstrings
-            fizzbuzz_temp.push_str(&format!("({}) buzz", num_str));
+            fizzbuzz_temp.push_str(" FizzBuzz ");
         } else if num % 3 == 0 {
-            let num_str: &str = &num.to_string();
-            fizzbuzz_temp.push('\n');
-            // fizzbuzz_temp.push_str(f!("({num_str}) fizz")); // using fstrings
-            fizzbuzz_temp.push_str(&format!("({}) fizz", num_str));
+            fizzbuzz_temp.push_str(" Fizz ");
+        } else if num % 5 == 0 {
+            fizzbuzz_temp.push_str(" Buzz ");
         } else {
-            let num_str: &str = &num.to_string();
-            fizzbuzz_temp.push('\n');
-            fizzbuzz_temp.push_str(num_str);
+            fizzbuzz_temp.push_str(&format!(" {} ", &num.to_string()));
         }
     }
-    return fizzbuzz_temp;
+    return fizzbuzz_temp.trim().to_string();
 }
 
 fn main() {
