a5ecb784f90b704bfe9b5a5c6b3a2f6f306bb9c5
Muhammad Naufal <mnaufal0999@gmail.com>
2023-01-20 15:25:00 +0700

    refactor PigLatin

--- PigLatin/PigLatin.py
+++ PigLatin/PigLatin.py
@@ -1,5 +1,5 @@
 def PigLatin(s):
-    return " ".join(list(map(lambda x: f"{x[-1]}" if x[-1] in ['!', '?'] else f"{x[1:]}{x[0]}ay", s.split(" "))))
+    return " ".join(map(lambda x: x[-1] if not x[-1].isalnum() else f"{x[1:]}{x[0]}ay", s.split(" ")))
 
 print(PigLatin('Pig latin is cool'))
 print(PigLatin('This is my string'))
