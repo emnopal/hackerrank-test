e21d48545683a24a5d9df1f3cb623464964d3e4a
Muhammad Naufal <mnaufal0999@gmail.com>
2023-02-01 11:01:50 +0700

    add new algo: one line without recursion by using reduce

--- Factorial/Factorial_test.py
+++ Factorial/Factorial_test.py
@@ -1,5 +1,6 @@
 import unittest
 import logging
+from functools import reduce
 
 log = logging.getLogger(__name__)
 
@@ -21,11 +22,17 @@ def FactorialRecursionOneLine(num: int) -> int:
     return 1 if num < 1 else num * FactorialRecursion(num - 1)
 
 
+def FactorialOneLineWithoutRecursion(num: int) -> int:
+    return 1 if num <= 1 else reduce(lambda x,y: x*y, range(1, num+1))
+
+
 class TestFactorial(unittest.TestCase):
 
     def setUp(self):
         loglevel = logging.DEBUG
         logging.basicConfig(level=loglevel, format="%(filename)s:%(lineno)s:%(funcName)20s() %(message)s")
+
+    # Normal Factorial
 
     def test_EasiestFactorial_normal(self):
         log.debug(EasiestFactorial(5))
@@ -48,6 +55,15 @@ class TestFactorial(unittest.TestCase):
             "Should equal 120"
         )
 
+    def test_FactorialOneLineWithoutRecursion_normal(self):
+        log.debug(FactorialOneLineWithoutRecursion(5))
+        self.assertEqual(
+            FactorialOneLineWithoutRecursion(5), 1*2*3*4*5,
+            "Should equal 120"
+        )
+
+    # Factorial of 1
+
     def test_EasiestFactorial_one(self):
         log.debug(EasiestFactorial(1))
         self.assertEqual(
@@ -68,6 +84,15 @@ class TestFactorial(unittest.TestCase):
             FactorialRecursionOneLine(1), 1,
             "Should equal 1"
         )
+
+    def test_FactorialOneLineWithoutRecursion_one(self):
+        log.debug(FactorialOneLineWithoutRecursion(1))
+        self.assertEqual(
+            FactorialOneLineWithoutRecursion(1), 1,
+            "Should equal 1"
+        )
+
+    # Special case: Factorial of Zero
 
     def test_EasiestFactorial_zero(self):
         log.debug(EasiestFactorial(0))
@@ -90,6 +115,15 @@ class TestFactorial(unittest.TestCase):
             "Should equal 1"
         )
 
+    def test_FactorialOneLineWithoutRecursion_zero(self):
+        log.debug(FactorialOneLineWithoutRecursion(0))
+        self.assertEqual(
+            FactorialOneLineWithoutRecursion(0), 1,
+            "Should equal 1"
+        )
+
+    # Big num factorial
+
     def test_EasiestFactorial_bigNum(self):
         log.debug(EasiestFactorial(20))
         self.assertEqual(
@@ -108,6 +142,13 @@ class TestFactorial(unittest.TestCase):
         log.debug(FactorialRecursionOneLine(20))
         self.assertEqual(
             FactorialRecursionOneLine(20), 1*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20,
+            "Should equal 2432902008176640000"
+        )
+
+    def test_FactorialOneLineWithoutRecursion_bigNum(self):
+        log.debug(FactorialOneLineWithoutRecursion(20))
+        self.assertEqual(
+            FactorialOneLineWithoutRecursion(20), 1*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20,
             "Should equal 2432902008176640000"
         )
 
