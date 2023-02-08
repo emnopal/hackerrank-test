d9c754ca22f3ec7d184abe76cc66bf25329950fb
Muhammad Naufal <mnaufal0999@gmail.com>
2023-02-08 17:04:24 +0700

    [ONGOING] implement binarytree

---
+++ BinaryTree/BinaryTree.go
@@ -0,0 +1,144 @@
+package main
+
+import "fmt"
+
+type NumTypes interface {
+	int | int64 | float32 | float64 | uint | uint64
+}
+
+type Node[Numeric NumTypes] struct {
+	data  Numeric
+	left  *Node[Numeric]
+	right *Node[Numeric]
+}
+
+type Tree[Numeric NumTypes] struct {
+	root *Node[Numeric]
+}
+
+type Options struct {
+	order string
+}
+
+func (node *Node[Numeric]) insert(data Numeric) {
+
+	if node.data == data {
+		return
+	}
+
+	if data < node.data {
+		if node.left != nil {
+			node.left.insert(data)
+		} else {
+			node.left = &Node[Numeric]{data, nil, nil}
+		}
+	} else {
+		if node.right != nil {
+			node.right.insert(data)
+		} else {
+			node.right = &Node[Numeric]{data, nil, nil}
+		}
+	}
+
+}
+
+func (node *Node[Numeric]) find(data Numeric) bool {
+
+	if data == node.data {
+		return true
+	}
+
+	if data < node.data {
+		if node.left != nil {
+			return node.left.find(data)
+		}
+		return false
+	}
+
+	if node.right != nil {
+		return node.right.find(data)
+	}
+	return false
+
+}
+
+func (node *Node[Numeric]) show(options *Options) {
+
+	if node != nil {
+
+		if options.order == "in" {
+			if node.left != nil {
+				node.left.show(options)
+			}
+			fmt.Print(node.data, " ")
+			if node.right != nil {
+				node.right.show(options)
+			}
+			return
+		}
+
+		if options.order == "post" {
+			if node.left != nil {
+				node.left.show(options)
+			}
+			if node.right != nil {
+				node.right.show(options)
+			}
+			fmt.Print(node.data, " ")
+			return
+		}
+
+		fmt.Print(node.data, " ")
+		if node.left != nil {
+			node.left.show(options)
+		}
+		if node.right != nil {
+			node.right.show(options)
+		}
+		return
+
+	}
+
+}
+
+func (tree *Tree[Numeric]) insert(data Numeric) *Tree[Numeric] {
+	if tree.root != nil {
+		tree.root.insert(data)
+	} else {
+		tree.root = &Node[Numeric]{data, nil, nil}
+	}
+	return tree
+}
+
+func (tree *Tree[Numeric]) find(data Numeric) bool {
+	var node *Node[Numeric] = tree.root
+	if node != nil {
+		return node.find(data)
+	}
+	return false
+}
+
+func (tree *Tree[Numeric]) show(options *Options) {
+	var node *Node[Numeric] = tree.root
+	fmt.Print(options.order, ": ")
+	if node != nil {
+		node.show(options)
+	}
+	fmt.Println()
+}
+
+func main() {
+	var arr []int = []int{10, 12, 5, 4, 20, 8, 7, 15, 13}
+	var tree Tree[int] = Tree[int]{root: nil}
+
+	for _, i := range arr {
+		tree.insert(i)
+	}
+
+	fmt.Println(tree.find(1))
+	fmt.Println(tree.find(12))
+
+	tree.show(&Options{order: "pre"})
+	tree.show(&Options{order: "in"})
+	tree.show(&Options{order: "post"})
+}
---
+++ BinaryTree/BinaryTree.py
@@ -0,0 +1,113 @@
+from __future__ import annotations
+from typing import TypeVar
+
+Numeric = TypeVar("Numeric", int, float)
+
+
+class Node:
+    def __init__(self, data: Numeric) -> None:
+        self.data: Numeric = data
+        self.left: Node = None
+        self.right: Node = None
+
+    def insert(self, data: Numeric):
+
+        if self.data == data:
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.insert(data)
+            else:
+                self.left = Node(data)
+        else:
+            if self.right:
+                self.right.insert(data)
+            else:
+                self.right = Node(data)
+
+
+    def find(self, data) -> bool:
+        if data == self.data:
+            return True
+        if data < self.data:
+            if self.left:
+                self.left.find(data)
+            return False
+        if self.right:
+            self.right.find(data)
+        return False
+
+    def show(self, order='pre') -> None:
+        if self:
+
+            if order == 'in':
+                if self.left:
+                    self.left.show(order)
+                print(str(self.data), end = ' ')
+                if self.right:
+                    self.right.show(order)
+                return
+
+            if order == 'post':
+                if self.left:
+                    self.left.show(order)
+                if self.right:
+                    self.right.show(order)
+                print(str(self.data), end = ' ')
+                return
+
+            print(str(self.data), end = ' ')
+            if self.left:
+                self.left.show(order)
+            if self.right:
+                self.right.show(order)
+            return
+
+
+class Tree:
+    def __init__(self) -> None:
+        self.root: Node = None
+
+    def insert(self, data: Numeric) -> Tree:
+        if self.root:
+            return self.root.insert(data)
+        self.root = Node(data)
+        return self
+
+    def find(self, data: Numeric) -> bool:
+        if self.root:
+            return self.root.find(data)
+        return False
+
+    def show(self, order='pre') -> None:
+        if self.root:
+            print(f'\n\n{order.capitalize()}order: ')
+            self.root.show(order)
+
+if __name__ == '__main__':
+    tree = Tree()
+
+    arr = [10,12,5,4,20,8,7,15,13]
+
+    for i in arr:
+        tree.insert(i)
+
+    ''' This will create:
+                    10
+                 /      \
+               5         12
+              / \           \
+            4     8          20
+                 /          /
+                7         15
+                         /
+                       13
+    '''
+
+    print(tree.find(1))
+    print(tree.find(12))
+
+    tree.show('pre')
+    tree.show('in')
+    tree.show('post')
