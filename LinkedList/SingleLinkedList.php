e3ce737a01b068fc690c00fe7decbb408604ed74
Muhammad Naufal <mnaufal0999@gmail.com>
2023-02-10 11:23:37 +0700

    refactoring

--- LinkedList/SingleLinkedList.php
+++ LinkedList/SingleLinkedList.php
@@ -1,135 +1,137 @@
 <?php
 
-class Node{
-    public mixed $data;
-    public ?Node $next;
+namespace SingleLinkedList {
+    class Node{
+        public mixed $data;
+        public ?Node $next;
 
-    public function __construct(mixed $data) {
-        $this->data = $data;
-        $this->next = null;
+        public function __construct(mixed $data) {
+            $this->data = $data;
+            $this->next = null;
+        }
+
     }
 
-}
+    class LinkedList {
+        private ?Node $head;
+        public int $length;
 
-class LinkedList {
-    private ?Node $head;
-    public int $length;
+        public function __construct()
+        {
+            $this->head = null;
+            $this->length = 0;
+        }
 
-    public function __construct()
-    {
-        $this->head = null;
-        $this->length = 0;
-    }
+        public function append(mixed $data, ?string $options = 'end'): void {
+            $node = new Node($data);
 
-    public function append(mixed $data, ?string $options = 'end'): void {
-        $node = new Node($data);
+            if ($options == 'front') {
+                $node->next = $this->head;
+                $this->head = $node;
+                $this->length++;
+                return;
+            }
 
-        if ($options == 'front') {
-            $node->next = $this->head;
-            $this->head = $node;
+            if (!$this->head) {
+                $this->head = $node;
+                $this->length++;
+                return;
+            }
+
+            $temp = $this->head;
+            while($temp->next) {
+                $temp = $temp->next;
+            }
+            $temp->next = $node;
             $this->length++;
             return;
+
         }
 
-        if (!$this->head) {
-            $this->head = $node;
-            $this->length++;
-            return;
+        public function reverse(): Node {
+            $reverseLL = null;
+            $temp = $this->head;
+            while($temp) {
+                $next_node = $temp->next;
+                $temp->next = $reverseLL;
+                $reverseLL = $temp;
+                $temp = $next_node;
+            }
+            return $reverseLL;
         }
 
-        $temp = $this->head;
-        while($temp->next) {
-            $temp = $temp->next;
+        public function print(): void {
+            $node = $this->head;
+            while($node) {
+                echo $node->data .' ';
+                $node = $node->next;
+            }
         }
-        $temp->next = $node;
-        $this->length++;
-        return;
+
+        public function printReverse(): void {
+            $node = $this->reverse();
+            while($node) {
+                echo $node->data .' ';
+                $node = $node->next;
+            }
+        }
+
+        public function size(): int {
+            return $this->length;
+        }
 
     }
 
-    public function reverse(): Node {
-        $reverseLL = null;
-        $temp = $this->head;
-        while($temp) {
-            $next_node = $temp->next;
-            $temp->next = $reverseLL;
-            $reverseLL = $temp;
-            $temp = $next_node;
+    function main(): void {
+
+        $ll = new LinkedList();
+        $lld = new LinkedList();
+        $lla = new LinkedList();
+        $llf = new LinkedList();
+
+        $arr = [6,4,3,1,10,8,9,12,0];
+
+        for($i = 0; $i <= 5; $i++) {
+            $ll->append($i);
+            $lld->append($i, options: 'front');
         }
-        return $reverseLL;
-    }
 
-    public function print(): void {
-        $node = $this->head;
-        while($node) {
-            echo $node->data .' ';
-            $node = $node->next;
+        foreach($arr as $i) {
+            $lla->append($i);
+            $llf->append($i, options: 'front');
         }
+
+        $ll->print();
+        echo PHP_EOL;
+        $ll->printReverse();
+        echo PHP_EOL .$ll->length .PHP_EOL;
+        echo $ll->size() .PHP_EOL;
+
+        echo PHP_EOL;
+
+        $lld->print();
+        echo PHP_EOL;
+        $lld->printReverse();
+        echo PHP_EOL .$lld->length .PHP_EOL;
+        echo $lld->size() .PHP_EOL;
+
+        echo PHP_EOL;
+
+        $lla->print();
+        echo PHP_EOL;
+        $lla->printReverse();
+        echo PHP_EOL .$lla->length .PHP_EOL;
+        echo $lla->size() .PHP_EOL;
+
+        echo PHP_EOL;
+
+        $llf->print();
+        echo PHP_EOL;
+        $llf->printReverse();
+        echo PHP_EOL .$llf->length .PHP_EOL;
+        echo $llf->size() .PHP_EOL;
+
     }
 
-    public function printReverse(): void {
-        $node = $this->reverse();
-        while($node) {
-            echo $node->data .' ';
-            $node = $node->next;
-        }
-    }
-
-    public function size(): int {
-        return $this->length;
-    }
-
+    main();
 }
-
-function main(): void {
-
-    $ll = new LinkedList();
-    $lld = new LinkedList();
-    $lla = new LinkedList();
-    $llf = new LinkedList();
-
-    $arr = [6,4,3,1,10,8,9,12,0];
-
-    for($i = 0; $i <= 5; $i++) {
-        $ll->append($i);
-        $lld->append($i, options: 'front');
-    }
-
-    foreach($arr as $i) {
-        $lla->append($i);
-        $llf->append($i, options: 'front');
-    }
-
-    $ll->print();
-    echo PHP_EOL;
-    $ll->printReverse();
-    echo PHP_EOL .$ll->length .PHP_EOL;
-    echo $ll->size() .PHP_EOL;
-
-    echo PHP_EOL;
-
-    $lld->print();
-    echo PHP_EOL;
-    $lld->printReverse();
-    echo PHP_EOL .$lld->length .PHP_EOL;
-    echo $lld->size() .PHP_EOL;
-
-    echo PHP_EOL;
-
-    $lla->print();
-    echo PHP_EOL;
-    $lla->printReverse();
-    echo PHP_EOL .$lla->length .PHP_EOL;
-    echo $lla->size() .PHP_EOL;
-
-    echo PHP_EOL;
-
-    $llf->print();
-    echo PHP_EOL;
-    $llf->printReverse();
-    echo PHP_EOL .$llf->length .PHP_EOL;
-    echo $llf->size() .PHP_EOL;
-
-}
-
-main();
