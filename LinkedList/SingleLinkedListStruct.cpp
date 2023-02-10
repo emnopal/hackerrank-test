2c58df816120eae4f194ba73b57e01997f42a5bc
Muhammad Naufal <mnaufal0999@gmail.com>
2023-02-10 11:23:55 +0700

    refactoring

--- LinkedList/SingleLinkedListStruct.cpp
+++ LinkedList/SingleLinkedListStruct.cpp
@@ -1,47 +1,53 @@
 #include <iostream>
 #include <string>
 
-struct Node {
+struct Node
+{
     int data;
-    Node* next;
+    Node *next;
 };
 
-struct LinkedList {
-    Node* head;
+struct LinkedList
+{
+    Node *head;
     int length;
 };
 
-void append(LinkedList *LL, int data, std::string position = "end") {
-    Node* newNode = new Node{
+void append(LinkedList *LL, int data, std::string position = "end")
+{
+    Node *newNode = new Node{
         data,
-        NULL
-    };
+        NULL};
 
-    if (position == "front") {
+    if (position == "front")
+    {
         newNode->next = LL->head;
         LL->head = newNode;
         LL->length++;
         return;
     }
 
-    if (!LL->head) {
+    if (!LL->head)
+    {
         LL->head = newNode;
         LL->length++;
         return;
     }
 
-    Node* temp = LL->head;
-    while(temp->next != nullptr) {
+    Node *temp = LL->head;
+    while (temp->next != nullptr)
+    {
         temp = temp->next;
     }
     temp->next = newNode;
     LL->length++;
-
 }
 
-Node *reverse(LinkedList *LL) {
+Node *reverse(LinkedList *LL)
+{
     Node *reverseLL = nullptr, *next_node, *temp = LL->head;
-    while(temp != nullptr) {
+    while (temp != nullptr)
+    {
         next_node = temp->next;
         temp->next = reverseLL;
         reverseLL = temp;
@@ -50,44 +56,51 @@ Node *reverse(LinkedList *LL) {
     return reverseLL;
 }
 
-void print(LinkedList *LL) {
-    Node* temp = LL->head;
-    while(temp != nullptr) {
+void print(LinkedList *LL)
+{
+    Node *temp = LL->head;
+    while (temp != nullptr)
+    {
         std::cout << temp->data << " ";
         temp = temp->next;
     }
 }
 
-void reversePrint(LinkedList *LL) {
-    Node* temp = reverse(LL);
-    while(temp != nullptr) {
+void reversePrint(LinkedList *LL)
+{
+    Node *temp = reverse(LL);
+    while (temp != nullptr)
+    {
         std::cout << temp->data << " ";
         temp = temp->next;
     }
 }
 
-int size(LinkedList *LL) {
+int size(LinkedList *LL)
+{
     return LL->length;
 }
 
-int main(){
+int main()
+{
     LinkedList *ll = new LinkedList{};
     LinkedList *lld = new LinkedList{};
     LinkedList *lla = new LinkedList{};
     LinkedList *llf = new LinkedList{};
 
-    int arr[] = {6,4,3,1,10,8,9,12,0};
+    int arr[] = {6, 4, 3, 1, 10, 8, 9, 12, 0};
 
-    for(int i = 0; i <= 10; i++) {
+    for (int i = 0; i <= 10; i++)
+    {
         append(ll, i);
         append(lld, i, "front");
     }
 
-    for(auto i: arr) {
+    for (auto i : arr)
+    {
         append(lla, i);
         append(llf, i, "front");
     }
-
 
     print(ll);
     reversePrint(ll);
